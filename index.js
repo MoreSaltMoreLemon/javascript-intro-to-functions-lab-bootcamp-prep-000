2896c32302a07bc89df3b5a69b6bb4076dea1fec
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 03:10:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 function shout(string) {
   return string.toUpperCase();
 }
+
+function whisper(string) {
+  return string.toLowercase();
+}
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return "I can't hear you!";
+  } else if (string === uppercase) {
+    return "YES INDEED!";
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
